b72952f0943a08fc84e5267d41a0519ecbe1d93f
nmjr-311 <nmjr31@gmail.com>
2019-11-17 14:17:42 +0900

    wip: Add exampleSpec

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,44 +1,74 @@
 module Parser
-  ( lit
+  ( yaml
+  , lit
   , litStr
   , litNum
   , key
   , pair
+  , mapping
+  , sequence
   , withText
   )where
 
 import           Control.Applicative
+import           Data.Functor.Identity  (Identity)
 import qualified Data.Text              as T (Text, pack, strip)
-import qualified Text.Parsec            as P (ParseError, runParser, try)
+import           Prelude                hiding (sequence)
+import qualified Text.Parsec            as P
 import           Text.Parsec.Char
-import           Text.Parsec.Combinator
-import qualified Text.Parsec.Text       as T
+import           Text.Parsec.Combinator (eof, many1, manyTill)
 
 import           Types
 
-colon :: T.Parser ()
-colon = spaces *> char ':' *> spaces
+-- indent size
+newtype ISize = ISize Int deriving (Eq, Show, Ord)
 
-newline' :: T.Parser ()
-newline' = newline *> pure ()
+type ParserT m a = P.ParsecT T.Text ISize m a
+type Parser a = ParserT Identity a
+type YamlParserT m = ParserT m Yaml
+type YamlParser = YamlParserT Identity
 
-yaml :: T.Parser Yaml
-yaml = lit
+validChar :: Parser Char
+validChar = satisfy (`notElem` (":-\n" :: String))
 
-lit :: T.Parser Yaml
+indent :: Parser ()
+indent = fmap length (many space) >>= P.setState . ISize
+
+colon :: Parser ()
+colon = char ':' *> (spaces <|> newline')
+
+hyphen :: Parser ()
+hyphen = char '-' *> (spaces <|> newline')
+
+newline' :: Parser ()
+newline' = () <$ (newline <|> char '\n' <|> endOfLine) *> indent
+
+yaml :: YamlParser
+yaml = P.try sequence <|> P.try mapping <|> lit
+
+lit :: YamlParser
 lit = P.try litNum <|> litStr
 
-litStr :: T.Parser Yaml
-litStr = LitStr . T.strip . T.pack <$> manyTill (satisfy (/= ':')) (newline' <|> eof)
+litStr :: YamlParser
+litStr = LitStr <$> (P.try quote <|> normal)
+  where
+    normal = T.strip . T.pack <$> manyTill validChar (newline' <|> eof)
+    quote = T.pack <$> (char '"' *> manyTill anyChar (char '"') <* (newline' <|> eof))
 
-litNum :: T.Parser Yaml
+litNum :: YamlParser
 litNum = LitNum . read <$> manyTill digit (newline' <|> eof)
 
-key :: T.Parser Key
-key = Key . T.strip . T.pack <$> manyTill alphaNum colon
+key :: Parser Key
+key = Key . T.strip . T.pack <$> manyTill validChar colon
 
-pair :: T.Parser (Key, Yaml)
+pair :: Parser (Key, Yaml)
 pair = liftA2 (,) key yaml
 
-withText :: T.Parser a -> T.Text -> Either P.ParseError a
-withText p t = P.runParser p () "text" t
+mapping :: YamlParser
+mapping = Mapping <$> many1 pair
+
+sequence :: YamlParser
+sequence = Sequence <$> many1 (hyphen *> yaml)
+
+withText :: Parser a -> T.Text -> Either P.ParseError a
+withText p = P.runParser p (ISize 0) "text"
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,14 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module ParserSpec
   ( spec
   ) where
 
+import qualified Data.Text  as T (concat)
 import           Parser
+import           Prelude    hiding (sequence)
 import           Test.Hspec
 import           Types
 
 -- all specs
 spec :: Spec
-spec = litStrSpec *> litNumSpec *> litSpec *> keySpec *> pairSpec
+spec =
+  litStrSpec *> litNumSpec
+  *> litSpec *> keySpec
+  *> pairSpec *> mappingSpec
+  *> sequenceSpec *> exampleSpec
 
 litSpec :: Spec
 litSpec =
@@ -37,6 +44,9 @@ litStrSpec =
     it "should return trimmed string" $
       withText litStr " abc " `shouldBe` Right (LitStr "abc")
 
+    it "should parse double quotation style" $
+      withText litStr "\" a:bc\ndef\n   gef \"" `shouldBe` Right (LitStr " a:bc\ndef\n   gef ")
+
 litNumSpec :: Spec
 litNumSpec =
   describe "litNum" $
@@ -52,8 +62,8 @@ keySpec =
     it "should parse string even if text contains numbers" $
       withText key "ke123y : value" `shouldBe` Right (Key "ke123y")
 
-    it "should fail when key contains spaces" $
-      withText key "ke y : value" `shouldSatisfy` parseFail
+    it "should parse string even if text contains spaces" $
+      withText key "ke  y : value" `shouldBe` Right (Key "ke  y")
 
 pairSpec :: Spec
 pairSpec =
@@ -67,7 +77,85 @@ pairSpec =
   it "should parse (key, string) pair even if string contains spaces" $
     withText pair "key  :   ab  cd 12 df  " `shouldBe` Right (Key "key", LitStr "ab  cd 12 df")
 
-parseFail :: Either l r -> Bool
-parseFail = \case
-  Left _ -> True
-  Right _ -> False
+mappingSpec :: Spec
+mappingSpec =
+  describe "mapping" $ do
+  it "should parse singleton (key: value) pairg" $
+    withText mapping "key1: value1" `shouldBe` Right (Mapping [(Key "key1", LitStr "value1")])
+
+  it "should parse (key: value) pairs" $
+    withText mapping "key1: value1 \nkey2: 987" `shouldBe`
+    Right (Mapping [(Key "key1", LitStr "value1"), (Key "key2", LitNum 987)])
+
+  it "should parse (key: sequence) pair" $
+    withText mapping "key:\n  - value1\n  - 111\n - 2aa\n" `shouldBe`
+    Right (Mapping [(Key "key", Sequence [LitStr "value1", LitNum 111, LitStr "2aa"])])
+
+sequenceSpec :: Spec
+sequenceSpec =
+  describe "sequence" $ do
+  it "should parse singleton sequence" $
+    withText sequence "- a" `shouldBe` Right (Sequence [LitStr "a"])
+
+  it "should parse sequence of literals" $
+    withText sequence "- a\n- 1" `shouldBe` Right (Sequence [LitStr "a", LitNum 1])
+
+exampleSpec :: Spec
+exampleSpec =
+  describe "examples of valid YAMLs" $ do
+    it "sequence of scalars" $
+      withText yaml "- aaa\n- bbb\n- 123"
+      `shouldBe` Right (Sequence [LitStr "aaa", LitStr "bbb", LitNum 123])
+    it "mapping scalars to sequences" $
+      withText yaml
+      (T.concat [ "a:\n  - b\n  - 12\n  - c\n"
+                , "d:\n  - e\n  - 34\n  - f"])
+      `shouldBe`
+      Right (Mapping [ ( Key "a"
+                       , Sequence [ LitStr "b"
+                                  , LitNum 12
+                                  , LitStr "c"])
+                     , ( Key "d"
+                       , Sequence [ LitStr "e"
+                                  , LitNum 34
+                                  , LitStr "f"])])
+    it "sequence of mappings" $
+      withText yaml
+      (T.concat [ "-\n  a: b1\n  c:  11\n"
+                , "-\n  a: b2\n  c:  22"])
+      `shouldBe`
+      Right (Sequence [ Mapping [ (Key "a", LitStr "b1")
+                                , (Key "c", LitNum 11) ]
+                      , Mapping [ (Key "a", LitStr "b2")
+                                , (Key "c", LitNum 22) ] ])
+    it "complex structure" $
+      withText yaml
+      (T.concat [ "number: 1234\n"
+                , "string: string\n"
+                , "mapOfMap: \n"
+                , "  attr1: attr1\n"
+                , "  attr2: attr2\n"
+                , "  map:\n"
+                , "    quotes:\n"
+                , "      \"abc\nspace \""
+                , "    attr1 : abc\n"
+                , "seqMap:\n"
+                , "  - elem1: 123\n"
+                , "    elem2: abc\n"
+                , "  - elem1: 456\n"
+                , "    elem2: def\n"
+                , "fin: \"122\""]) `shouldBe`
+      Right (Mapping [ (Key "number", LitNum 1234)
+                     , (Key "string", LitStr "string")
+                     , ( Key "mapOfMap"
+                       , Mapping [ (Key "attr1", LitStr "attr1")
+                                 , (Key "attr2", LitStr "attr2")
+                                 , (Key "map", Mapping [ ( Key "quotes"
+                                                         , LitStr "abc\nspace ")
+                                                         , (Key "attr1", LitStr "abc")])])
+                   , ( Key "seqMap"
+                     , Sequence [ Mapping [ (Key "elem1", LitNum 123)
+                                          , (Key "elem2", LitStr "abc")]
+                                , Mapping [ (Key "elem1", LitNum 456)
+                                          , (Key "elem2", LitStr "def")]])
+                   , (Key "fin", LitStr "122")])
